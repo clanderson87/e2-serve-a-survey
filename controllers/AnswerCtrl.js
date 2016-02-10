930e6159812a1cdcde667df296a8afc9f26385b8
Chris Anderson <cl.anderson8@gmail.com>
2016-02-09 20:58:53 -0600

    work on answer list. Trying to display questions.

--- controllers/AnswerCtrl.js
+++ controllers/AnswerCtrl.js
@@ -1,8 +1,36 @@
 app.controller('AnswerCtrl',
-  [
-    function(){
+  ['$firebaseArray',
+  '$firebaseObject',
+
+    function(
+      $firebaseArray,
+      $firebaseObject){
+
+        //alising this
+        var vm = this;
+
+        //accessing firebase
+        var questionRef = new Firebase('https://survey-creator.firebaseio.com/questions');
+
+        var list = $firebaseArray(questionRef);
+
+        vm.getFirebaseList = function(){
+          console.log(list)
+          list.$loaded()
+            .then(function(questionList) {
+              var x = Math.floor((Math.random() * questionList.length - 1) + 1);
+              var pumpkin = questionList[x];
+              console.log("x is ", x);
+              console.log("pumpkin is ", pumpkin);
+              console.log(Object.keys(pumpkin));
+
+          })
+            .catch(function(error) {
+              console.log("Error:", error);
+            }
+          );
+        }();
 
     }
-
   ]
 )
