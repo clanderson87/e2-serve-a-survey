d28f44d00153ff6181e8a2ffccf648907971e62a
Chris Anderson <cl.anderson8@gmail.com>
2016-02-10 18:12:51 -0600

    adjusting aCtrl and loginTmpl to display saveSurvey button

--- controllers/AuthCtrl.js
+++ controllers/AuthCtrl.js
@@ -1,19 +1,24 @@
 app.controller('AuthCtrl',
     ["$location",
         function($location){
+                //aliasing this as vm
+                var vm = this;
+                //firebase reference
                 var ref = new Firebase("https://survey-creator.firebaseio.com");
-                this.login = function(){
+                //login function
+                vm.login = function(){
                     ref.authWithOAuthPopup("google", function(error, authData) {
                         if (error) {
                             console.log("Login Failed", error);
                         } else {
                             console.log("Authenticated successfully with:", authData);
-                            userRefObj = authData;
+                            userRefObj = vm.authData;
                             userRefObj.username = authData.google.displayName;
                             ref.child("users").child(authData.uid).set(userRefObj);
                         };
 
                     })
+                    //switiches view to survey creation
                     $location.path('/makeSurvey');
                 }
             }
