6d91d366a0173db045a86c0102e230f2432626d8
Chris Anderson <cl.anderson8@gmail.com>
2016-02-15 20:23:44 -0600

    Holy Comments batman

--- controllers/responseCtrl.js
+++ controllers/responseCtrl.js
@@ -41,24 +41,34 @@ app.controller('responseCtrl',
           var thisSurveyObj = ref.child('answers').child(authData.uid).child(surveyTitle).once('value', function(snapshot){
               var thisKeyArray = [];
               snapshot.forEach(function(childSnapshot){
+                //aliasing the value of the snapshot to childData. childData should be the 'question' object itself. Unfortunately, it doesn't have an accessable name/title property
                 var childData = childSnapshot.val()
                 console.log("childData is ", childData);
+                //getting the keys of childData;
                 var childDataKeys = Object.keys(childData);
                 console.log("childDataKeys is ", childDataKeys);
+                //pushing childDataKeys into thisKeyArray
                 thisKeyArray.push(childDataKeys);
                 console.log("thisKeyArray is ", thisKeyArray);
+                //instantiating accessable thisAnswerArray
                 vm.thisAnswerArray = []
+                //for Each Key in thisKeyArray
                 thisKeyArray.forEach(function(answerKey){
+                  console.log(answerKey);
+                  //iterate over each answerKey
                   answerKey.forEach(function(answerGetter){
+                    console.log(childData);
+                    //calling question.answerGetter to access actual answer, usually renders an array
                     var answerArray = (childData[answerGetter])
                     console.log("answerArray is ", answerArray)
+                    //for each item in answerArray, stringify the index and push it to accessable vm.thisAnswerArray
                     answerArray.forEach(function(answer){
                       vm.thisAnswerArray.push(answer.toString());
                       console.log("vm.thisAnswerArray is ", vm.thisAnswerArray);
-                    })
-                  });
-                })
-              })
+                    })//close answerArray.forEach
+                  })// close answerKey.forEach
+                })//close thisKeyArray.forEach
+              })//close snapshot.forEach
           });
           }
 
