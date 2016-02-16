64f21684d99d3d87075b0b7b297bef48e4cb5f4f
Chris Anderson <cl.anderson8@gmail.com>
2016-02-15 20:44:28 -0600

    Fuck it. We'll do it live.

--- controllers/responseCtrl.js
+++ controllers/responseCtrl.js
@@ -39,36 +39,63 @@ app.controller('responseCtrl',
 
         vm.displayResponses = function(surveyTitle){
           var thisSurveyObj = ref.child('answers').child(authData.uid).child(surveyTitle).once('value', function(snapshot){
+              //might not even need this:
               var thisKeyArray = [];
+              //instantiating accessable answerArray
+              vm.answerArray = []
               snapshot.forEach(function(childSnapshot){
-                //aliasing the value of the snapshot to childData. childData should be the 'question' object itself. Unfortunately, it doesn't have an accessable name/title property
+                //trying to get questionNames to appear. Works!
+                var questionName = childSnapshot.key();
+                console.log("questionName is ", questionName)
+                //aliasing the value of the snapshot to childData, which is the question
                 var childData = childSnapshot.val()
                 console.log("childData is ", childData);
-                //getting the keys of childData;
-                var childDataKeys = Object.keys(childData);
-                console.log("childDataKeys is ", childDataKeys);
-                //pushing childDataKeys into thisKeyArray
-                thisKeyArray.push(childDataKeys);
-                console.log("thisKeyArray is ", thisKeyArray);
-                //instantiating accessable thisAnswerArray
-                vm.thisAnswerArray = []
-                //for Each Key in thisKeyArray
-                thisKeyArray.forEach(function(answerKey){
-                  console.log(answerKey);
-                  //iterate over each answerKey
-                  answerKey.forEach(function(answerGetter){
-                    console.log(childData);
-                    //calling question.answerGetter to access actual answer, usually renders an array
-                    var answerArray = (childData[answerGetter])
-                    console.log("answerArray is ", answerArray)
-                    //for each item in answerArray, stringify the index and push it to accessable vm.thisAnswerArray
-                    answerArray.forEach(function(answer){
-                      vm.thisAnswerArray.push(answer.toString());
-                      console.log("vm.thisAnswerArray is ", vm.thisAnswerArray);
-                    })//close answerArray.forEach
-                  })// close answerKey.forEach
-                })//close thisKeyArray.forEach
-              })//close snapshot.forEach
+                //iterating over childData by each answerKey
+                for (answerKey in childData){
+                  //renders -Khex style answerKeys, good.
+                  console.log("answerKey is ", answerKey)
+                  //accessing and aliasing actual answer using childData and answerKey
+                  var childDataAnswer = (childData[answerKey])
+                  console.log("childDataAnswer is ", childDataAnswer)
+                  //pushing to accessable vm.answerArray
+                  vm.answerArray.push(childDataAnswer);
+                  console.log("vm.answerArray is ", vm.answerArray);
+
+
+                }
+              })
+
+              /* ---------- commenting this out for later OVER THOUGHT EVERYTHING?!??!?------
+              // snapshot.forEach(function(childSnapshot){
+              //   //aliasing the value of the snapshot to childData. childData should be the 'question' object itself. Unfortunately, it doesn't have an accessable name/title property
+              //   var childData = childSnapshot.val()
+              //   console.log("childData is ", childData);
+              //   //getting the keys of childData;
+              //   var childDataKeys = Object.keys(childData);
+              //   console.log("childDataKeys is ", childDataKeys);
+              //   //pushing childDataKeys into thisKeyArray
+              //   thisKeyArray.push(childDataKeys);
+              //   console.log("thisKeyArray is ", thisKeyArray);
+              //   //instantiating accessable thisAnswerArray
+              //   vm.thisAnswerArray = []
+              //   //for Each answerKey in thisKeyArray
+              //   thisKeyArray.forEach(function(answerKey){
+              //     console.log(answerKey);
+              //     //iterate over each answerKey
+              //     answerKey.forEach(function(answerGetter){
+              //       console.log(childData);
+              //       //calling question.answerGetter to access actual answer, usually renders an array
+              //       var answerArray = (childData[answerGetter])
+              //       console.log("answerArray is ", answerArray)
+              //       //for each item in answerArray, stringify the index and push it to accessable vm.thisAnswerArray
+              //       answerArray.forEach(function(answer){
+              //         vm.thisAnswerArray.push(answer.toString());
+              //         console.log("vm.thisAnswerArray is ", vm.thisAnswerArray);
+              //       })//close answerArray.forEach
+              //     })// close answerKey.forEach
+              //   })//close thisKeyArray.forEach
+              // })//close snapshot.forEach
+              ------------end overthought section ----------*/
           });
           }
 
