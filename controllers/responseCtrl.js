fedc6c3253cc6074ed8c6c379c8457843f5d6421
Chris Anderson <cl.anderson8@gmail.com>
2016-02-16 18:24:44 -0600

    getting somewhere with objects

--- controllers/responseCtrl.js
+++ controllers/responseCtrl.js
@@ -40,45 +40,47 @@ app.controller('responseCtrl',
           var thisSurveyObj = ref.child('answers').child(authData.uid).child(surveyTitle);
 
           thisSurveyObj.once('value', function(snapshot){
-            //instantiating accessable answerArray
-            vm.answerArray = []
-            //instantiating questionArray
-            var questionArray = [];
+            //making accessable questionObj
             vm.questionObj = {};
             snapshot.forEach(function(childSnapshot){
               //trying to get questionNames to appear. Works!
               var questionName = childSnapshot.key();
-
-              //snapshotCeption
-              thisSurveyObj.child(questionName).once('value', function(grandChildSnapshot){
-                //aliasing gCSnapshot as gCData
-                var grandChildData = grandChildSnapshot.val();
-                //making gCData accessable
-                vm.grandChildData = grandChildData
-                console.log("grandChildData is ", grandChildData);
-                //iterating over each answerKey in grandChildData
-                for (answerKey in grandChildData){
-                  //pushing qustionName
-                  if (questionArray.indexOf(questionName) < 1){
-                    questionArray.push(questionName);
-                  }
-                  console.log("questionArray is ", questionArray);
-                  //accessing and aliasing actual answer using gCData and answerKey
-                  var grandChildAnswer = (grandChildData[answerKey]);
-                  //instantiating packagable asset
-                  var asset = [];
-                  //pushing questionName and grandChildAnswer together in asset
-                  asset.push(questionName, grandChildAnswer)
-                  console.log("asset is ", asset)
-                  //pushing the asset into an accessable array
-                  vm.answerArray.push(asset);
-                  console.log("vm.answerArray is ", vm.answerArray);
+              //making childSnapshot usable
+              var childData = childSnapshot.val();
+              //adding childData as a property in accessable questionObj
+              vm.questionObj[questionName] = childData;
+            }).then(function(){
+              for (key in vm.questionObj){
+                console.log("vm.questionObj.key is ", key)
               }
-
             })
           })
-        })
-      }
+        }
 
 
 }])
+              // //snapshotCeption
+              // thisSurveyObj.child(questionName).once('value', function(grandChildSnapshot){
+              //   //aliasing gCSnapshot as gCData
+              //   var grandChildData = grandChildSnapshot.val();
+              //   //making gCData accessable
+              //   vm.grandChildData = grandChildData
+              //   console.log("grandChildData is ", grandChildData);
+              //   //iterating over each answerKey in grandChildData
+              //   for (answerKey in grandChildData){
+              //     //pushing qustionName
+              //     if (questionArray.indexOf(questionName) < 1){
+              //       questionArray.push(questionName);
+              //     }
+              //     console.log("questionArray is ", questionArray);
+              //     //accessing and aliasing actual answer using gCData and answerKey
+              //     var grandChildAnswer = (grandChildData[answerKey]);
+              //     //instantiating packagable asset
+              //     var asset = [];
+              //     //pushing questionName and grandChildAnswer together in asset
+              //     asset.push(questionName, grandChildAnswer)
+              //     console.log("asset is ", asset)
+              //     //pushing the asset into an accessable array
+              //     vm.answerArray.push(asset);
+              //     console.log("vm.answerArray is ", vm.answerArray);
+              // }
